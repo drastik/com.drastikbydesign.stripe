49c68b1689c93f230c84c45cf3e4a4d597d8b196
drastik <jwjoshuawalker@gmail.com>
2013-03-14 11:45:55 -0700

    Fix wrong datatype for  per github user metaxy

--- extension-4.2/com.drastikbydesign.stripe/CRM/Core/Payment/Stripe.php
+++ extension-4.2/com.drastikbydesign.stripe/CRM/Core/Payment/Stripe.php
@@ -472,7 +472,7 @@ class CRM_Core_Payment_Stripe extends CRM_Core_Payment {
     $query_params = array(
       1 => array($stripe_customer->id, 'String'),
       2 => array($invoice_id, 'String'),
-      3 => array($end_time, 'Timestamp'),
+      3 => array($end_time, 'Integer'),
     );
 
     // Insert the new Stripe Subscription info.

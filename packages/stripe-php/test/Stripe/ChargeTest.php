daba78c6f25b12fbd97646eddb442be23b479d2f
Laryn Kragt Bakker <laryn@cedc.org>
2016-02-04 14:29:15 -0600

    Updating to latest version of the Stripe PHP legacy bindings.

--- packages/stripe-php/test/Stripe/ChargeTest.php
+++ packages/stripe-php/test/Stripe/ChargeTest.php
@@ -30,6 +30,32 @@ class Stripe_ChargeTest extends StripeTestCase
     $this->assertFalse($c->refunded);
   }
 
+  public function testIdempotentCreate()
+  {
+    self::authorizeFromEnv();
+
+    $card = array(
+      'number' => '4242424242424242',
+      'exp_month' => 5,
+      'exp_year' => 2015
+    );
+
+    $c = Stripe_Charge::create(
+        array(
+          'amount' => 100,
+          'currency' => 'usd',
+          'card' => $card
+        ),
+        array(
+          'idempotency_key' => $this->generateRandomString(),
+        )
+    );
+
+    $this->assertTrue($c->paid);
+    $this->assertFalse($c->refunded);
+  }
+
+
   public function testRetrieve()
   {
     self::authorizeFromEnv();
@@ -85,7 +111,7 @@ class Stripe_ChargeTest extends StripeTestCase
       'exp_month' => 5,
       'exp_year' => 2015
     );
-    
+
     $charge = Stripe_Charge::create(
         array(
             'amount' => 100,
@@ -99,5 +125,76 @@ class Stripe_ChargeTest extends StripeTestCase
 
     $updatedCharge = Stripe_Charge::retrieve($charge->id);
     $this->assertEqual('foo bar', $updatedCharge->metadata['test']);
+  }
+
+  public function testMarkAsFraudulent()
+  {
+    self::authorizeFromEnv();
+
+    $card = array(
+      'number' => '4242424242424242',
+      'exp_month' => 5,
+      'exp_year' => 2015
+    );
+
+    $charge = Stripe_Charge::create(
+        array(
+            'amount' => 100,
+            'currency' => 'usd',
+            'card' => $card
+        )
+    );
+
+    $charge->refunds->create();
+    $charge->markAsFraudulent();
+
+    $updatedCharge = Stripe_Charge::retrieve($charge->id);
+    $this->assertEqual(
+        'fraudulent', $updatedCharge['fraud_details']['user_report']
+    );
+  }
+
+  public function testCreateWithBitcoinReceiverSource()
+  {
+    self::authorizeFromEnv();
+
+    $receiver = $this->createTestBitcoinReceiver("do+fill_now@stripe.com");
+
+    $charge = Stripe_Charge::create(
+        array(
+          'amount' => 100,
+          'currency' => 'usd',
+          'source' => $receiver->id
+        )
+    );
+
+    $this->assertEqual($receiver->id, $charge->source->id);
+    $this->assertEqual("bitcoin_receiver", $charge->source->object);
+    $this->assertEqual("paid", $charge->status);
+    $this->assertTrue(get_class($charge->source) == 'Stripe_BitcoinReceiver');
+  }
+
+  public function markAsSafe()
+  {
+    self::authorizeFromEnv();
+
+    $card = array(
+      'number' => '4242424242424242',
+      'exp_month' => 5,
+      'exp_year' => 2015
+    );
+
+    $charge = Stripe_Charge::create(
+        array(
+            'amount' => 100,
+            'currency' => 'usd',
+            'card' => $card
+        )
+    );
+
+    $charge->markAsSafe();
+
+    $updatedCharge = Stripe_Charge::retrieve($charge->id);
+    $this->assertEqual('safe', $updatedCharge['fraud_details']['user_report']);
   }
 }

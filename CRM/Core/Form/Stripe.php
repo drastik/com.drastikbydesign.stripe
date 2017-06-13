b69abd20be6da91fb56a44d8d4cc8f3370dcfb10
Jamie McClelland <jm@mayfirst.org>
2017-06-13 16:31:38 -0400

    Some objects in CiviCRM have this parameter set as public
    https://github.com/drastik/com.drastikbydesign.stripe/issues/218

--- CRM/Core/Form/Stripe.php
+++ CRM/Core/Form/Stripe.php
@@ -13,15 +13,28 @@ class CRM_Core_Form_Stripe extends CRM_Core_Form {
   public static function get_stripe_ppids(&$form, $live = 1) {
     $is_test = $live == 1 ? 0 : 1;
     $stripe_ppids = array();
-    foreach($form->_processors as $k => $v) {
-      $sql = "SELECT pp.id FROM civicrm_payment_processor pp JOIN 
-        civicrm_payment_processor_type ppt ON pp.payment_processor_type_id =
-        ppt.id AND ppt.name = 'Stripe' AND pp.is_active = 1 AND ppt.is_active = 1 
-        AND pp.id = %0 AND is_test = %1";
-      $params = array(0 => array($k, 'Integer'), 1 => array($is_test, 'Integer'));
-      $dao = CRM_Core_DAO::executeQuery($sql, $params);
-      if($dao->N == 1) {
-        $stripe_ppids[] = $k;
+    // On Contribution pages, this property is public, on membership pages it is
+    // private. Check if it is available.
+    if(isset($form->_paymentProcessors)) {
+      foreach($form->_paymentProcessors as $k => $v) {
+        if($v['object'] && $v['object']->_processorName == 'Stripe') {
+          if($v['object']->_islive == $live) {
+            $stripe_ppids[] = $k;
+          }
+        }
+      }
+    }
+    if(isset($form->_processors)) {
+      foreach($form->_processors as $k => $v) {
+        $sql = "SELECT pp.id FROM civicrm_payment_processor pp JOIN 
+          civicrm_payment_processor_type ppt ON pp.payment_processor_type_id =
+          ppt.id AND ppt.name = 'Stripe' AND pp.is_active = 1 AND ppt.is_active = 1 
+          AND pp.id = %0 AND is_test = %1";
+        $params = array(0 => array($k, 'Integer'), 1 => array($is_test, 'Integer'));
+        $dao = CRM_Core_DAO::executeQuery($sql, $params);
+        if($dao->N == 1) {
+          $stripe_ppids[] = $k;
+        }
       }
     }
     return $stripe_ppids;

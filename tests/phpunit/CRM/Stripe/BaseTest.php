10583e1ee9c84fd756165939b536f3d687c6b0e4
Jamie McClelland <jm@mayfirst.org>
2017-06-28 10:34:34 -0400

    adding tests for Ipn/recurring payments.

--- tests/phpunit/CRM/Stripe/BaseTest.php
+++ tests/phpunit/CRM/Stripe/BaseTest.php
@@ -4,6 +4,8 @@ use Civi\Test\HeadlessInterface;
 use Civi\Test\HookInterface;
 use Civi\Test\TransactionalInterface;
 
+define('STRIPE_PHPUNIT_TEST', 1);
+
 /**
  * FIXME - Add test description.
  *
@@ -27,7 +29,9 @@ class CRM_Stripe_BaseTest extends \PHPUnit_Framework_TestCase implements Headles
   protected $_contributionPageID;
   protected $_paymentProcessorID;
   protected $_paymentProcessor;
+  protected $_trxn_id;
 	protected $_created_ts;
+  protected $_subscriptionID;
 	// Secret/public keys are PTP test keys.
 	// protected $_sk = 'sk_test_TlGdeoi8e1EOPC3nvcJ4q5UZ';
 	// protected $_pk = 'pk_test_k2hELLGpBLsOJr6jZ2z9RaYh';
@@ -117,12 +121,89 @@ class CRM_Stripe_BaseTest extends \PHPUnit_Framework_TestCase implements Headles
       'max_amount' => 1000,
       'receipt_from_email' => 'gaia@the.cosmos',
       'receipt_from_name' => 'Pachamama',
-      'is_email_receipt' => TRUE,  
+      'is_email_receipt' => FALSE,  
       ), $params);
     $result = civicrm_api3('ContributionPage', 'create', $params);
 		$this->assertEquals(0, $result['is_error']);
     $this->_contributionPageID = $result['id']; 
   }
   
-  
+  /**
+   * Submit to stripe
+   */
+  public function doPayment($params = array()) {
+    $mode = 'test';
+    $pp = $this->_paymentProcessor;
+    $stripe = new CRM_Core_Payment_Stripe($mode, $pp);
+    $params = array_merge(array(
+      'payment_processor_id' => $this->_paymentProcessorID,
+      'amount' => $this->_total,
+      'stripe_token' => array(
+        'number' => '4111111111111111',
+        'exp_month' => '12',
+        'exp_year' => date('Y') + 1,
+        'cvc' => '123',
+        'name' => $this->contact->display_name,
+        'address_line1' => '123 4th Street',
+        'address_state' => 'NY',
+        'address_zip' => '12345',
+      ),
+      'email' => $this->contact->email,
+      'description' => 'Test from Stripe Test Code',
+      'currencyID' => 'USD',
+      'invoiceID' => $this->_invoiceID,
+    ), $params);
+
+    $ret = $stripe->doDirectPayment($params);
+    if (array_key_exists('trxn_id', $ret)) {
+      $this->_trxn_id = $ret['trxn_id'];
+    }
+    if (array_key_exists('subscription_id', $ret)) {
+      $this->_subscriptionID = $ret['subscription_id'];
+    }
+  }
+
+  /**
+   * Confirm that transaction id is legit and went through.
+   *
+   */
+  public function assertValidTrxn() {
+    $this->assertNotEmpty($this->_trxn_id, "A trxn id was assigned");
+
+    require_once('stripe-php/init.php');
+    \Stripe\Stripe::setApiKey($this->_sk);
+    $found = FALSE;
+    try {
+      $results = \Stripe\Charge::retrieve(array( "id" => $this->_trxn_id));
+      $found = TRUE;
+    }
+    catch (Stripe_Error $e) {
+      $found = FALSE;
+    }
+    
+    $this->assertTrue($found, 'Assigned trxn_id is valid.');
+
+  }  
+  /**
+   * Create contribition
+   */
+  public function setupTransaction($params = array()) {
+     $contribution = civicrm_api3('contribution', 'create', array_merge(array(
+      'contact_id' => $this->_contactID,
+      'contribution_status_id' => 2,
+      'payment_processor_id' => $this->_paymentProcessorID,
+      // processor provided ID - use contact ID as proxy.
+      'processor_id' => $this->_contactID,
+      'total_amount' => $this->_total,
+      'invoice_id' => $this->_invoiceID,
+      'financial_type_id' => $this->_financialTypeID,
+      'contribution_status_id' => 'Pending',
+      'contact_id' => $this->_contactID,
+      'contribution_page_id' => $this->_contributionPageID,
+      'payment_processor_id' => $this->_paymentProcessorID,
+      'is_test' => 1,
+    ), $params));
+		$this->assertEquals(0, $contribution['is_error']);
+    $this->_contributionID = $contribution['id'];
+  } 
 }
--- tests/phpunit/CRM/Stripe/IpnTest.php
+++ tests/phpunit/CRM/Stripe/IpnTest.php
@@ -20,8 +20,11 @@ use Civi\Test\TransactionalInterface;
  */
 require ('BaseTest.php');
 class CRM_Stripe_IpnTest extends CRM_Stripe_BaseTest {
-
+  protected $_total = '200';
   protected $_contributionRecurID;
+  protected $_installments = 5;
+  protected $_frequency_unit = 'month';
+  protected $_frequency_interval = 1;
 
   public function setUpHeadless() {
     // Civi\Test has many helpers, like install(), uninstall(), sql(), and sqlFile().
@@ -43,44 +46,66 @@ class CRM_Stripe_IpnTest extends CRM_Stripe_BaseTest {
    * Test making a recurring contribution.
    */
   public function testIPNRecurSuccess() {
-    $this->setupRecurringPaymentProcessorTransaction();
+    $this->setupRecurringTransaction();
+    $payment_extra_params = array(
+      'is_recur' => 1,
+      'contributionRecurID' => $this->_contributionRecurID,
+      'frequency_unit' => $this->_frequency_unit,
+      'frequency_interval' => $this->_frequency_interval,
+      'installments' => $this->_installments
+    );
+    $this->doPayment($payment_extra_params);
 
+    // Now check to see if an event was triggered and if so, process it.
+    // Get all events of the type invoice.payment_succeeded that have 
+    // happened since this code was invoked.
 		$params['sk'] = $this->_sk;
-		$params['created'] = array('gte' => $this->_created_ts - 86400);
+		$params['created'] = array('gte' => $this->_created_ts);
+    $params['type'] = 'invoice.payment_succeeded';
+
 		// Now try to retrieve this transaction.
 		$transactions = civicrm_api3('Stripe', 'listevents', $params );		
-		// print_r($params);
-    // print_r($transactions);
-		return;
-		$stripe = new CRM_Stripe_Page_Webhook();
-		$data = new stdClass();
-		$data->id = $this->_invoiceID;
-		$data->livemode = FALSE;
-		$stripe->run($data);
-    $contribution = civicrm_api3('contribution', 'getsingle', array('id' => $this->_contributionID));
-    $this->assertEquals(1, $contribution['contribution_status_id']);
+    $payment_object = NULL;
+    foreach($transactions['values']['data'] as $transaction) {
+      if ($transaction->data->object->subscription == $this->_subscriptionID) {
+        // This is the one.
+        $payment_object = $transaction;
+        break;
+      }
+    }
+
+    $contribution_status_id = NULL;
+    if ($payment_object) {
+      $stripe = new CRM_Stripe_Page_Webhook();
+      $stripe->run($payment_object);
+      $contribution = civicrm_api3('contribution', 'getsingle', array('id' => $this->_contributionID));
+      $contribution_status_id = $contribution['contribution_status_id'];
+    }
+    $this->assertEquals(1, $contribution_status_id, "Recurring payment was properly processed via a stripe event.");
   }
 
   /**
    * Create recurring contribition
    */
-  public function setupRecurringPaymentProcessorTransaction($params = array()) {
+  public function setupRecurringTransaction($params = array()) {
      $contributionRecur = civicrm_api3('contribution_recur', 'create', array_merge(array(
+      'financial_type_id' => $this->_financialTypeID,
+      'payment_instrument_id' => CRM_Core_OptionGroup::getValue('payment_instrument', 'Credit Card', 'name'),
       'contact_id' => $this->_contactID,
-      'amount' => 1000,
+      'amount' => $this->_total,
       'sequential' => 1,
-      'installments' => 5,
-      'frequency_unit' => 'Month',
-      'frequency_interval' => 1,
+      'installments' => $this->_installments,
+      'frequency_unit' => $this->_frequency_unit, 
+      'frequency_interval' => $this->_frequency_interval,
       'invoice_id' => $this->_invoiceID,
       'contribution_status_id' => 2,
       'payment_processor_id' => $this->_paymentProcessorID,
       // processor provided ID - use contact ID as proxy.
       'processor_id' => $this->_contactID,
       'api.contribution.create' => array(
-        'total_amount' => '200',
+        'total_amount' => $this->_total,
         'invoice_id' => $this->_invoiceID,
-        'financial_type_id' => 1,
+        'financial_type_id' => $this->_financialTypeID,
         'contribution_status_id' => 'Pending',
         'contact_id' => $this->_contactID,
         'contribution_page_id' => $this->_contributionPageID,
